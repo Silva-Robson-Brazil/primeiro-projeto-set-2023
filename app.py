7aa6bd80c9e146d4595bf0bc220f44b5679eb026
robson.pereira.silva <robson.pereira.silva@accenture.com>
2023-10-23 19:49:41 -0300

    Voltado nome de variável

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 def hello_world():
 
-    var = "Alteração"
+    var = "Nova alteração"
     status = "Verificar no git status" 
     print( f'"Foi adicionado uma {var}, E iremos {status}"Olá, Mundo!"')
