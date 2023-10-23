25d1b20e8bd0e854130cba9244855b32ac0228b2
robson.pereira.silva <robson.pereira.silva@accenture.com>
2023-10-23 13:30:46 -0300

    Foram alterados a variável var do "app.py"

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 def hello_world():
 
-    var = "Nova alteração"
+    var = "Alteração"
     status = "Verificar no git status" 
     print( f'"Foi adicionado uma {var}, E iremos {status}"Olá, Mundo!"')
